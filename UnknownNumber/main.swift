65bf6e4c032b8469d8b1af668fa80094563148e7
Dmitriy Danilin <danilindn@icloud.com>
2020-04-26 13:18:31 +0300

    Initial Commit

---
+++ UnknownNumber/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  UnknownNumber
+//
+//  Created by Дмитрий Данилин on 26.04.2020.
+//  Copyright © 2020 Дмитрий Данилин. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
